18a0c4b1ba2845b903e7ccff771a5dde643e18c4
Athil Althaf A Z <athilalthafaz0@gmail.com>
2024-03-05 13:46:50 +0100

    Create getWindowIntegrate.m
    sum outlier events based on  a time window

---
+++ getWindowIntegrate.m
@@ -0,0 +1,10 @@
+function window_sum_series = getWindowIntegrate(out_idx,time_window,fs)
+    
+    time_sample  = time2sample(time_window,fs);
+    unit_kernel = ones(1,time_sample);
+    window_sum_series = conv2(unit_kernel,1,out_idx,'same');
+
+    
+
+
+end
